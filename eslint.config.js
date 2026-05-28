2aa845dbefab663cffed6b068ce088004daf52b1
jaypolyg <ipolygenis@athtech.gr>
2026-05-28 04:39:53 +0300

    Add Jest plugin configuration to ESLint

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,23 @@
 import js from "@eslint/js";
 import globals from "globals";
+import jestPlugin from "eslint-plugin-jest";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.node } },
-  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: { globals: globals.node }
+  },
+  {
+    files: ["**/*.test.js"],
+    plugins: { jest: jestPlugin },
+    languageOptions: {
+      globals: globals.jest
+    },
+    rules: {
+      ...jestPlugin.configs.recommended.rules
+    }
+  }
 ]);
