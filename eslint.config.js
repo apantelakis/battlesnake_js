acfcd98058fecc1263e6dc420229e63c77a04034
Thanasis Pantelakis <pantelakis.thanasis@gmail.com>
2026-06-18 23:27:01 +0300

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -4,11 +4,15 @@ import jestPlugin from "eslint-plugin-jest";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
+  { ignores: ["docs/**"] },
   {
     files: ["**/*.{js,mjs,cjs}"],
     plugins: { js },
     extends: ["js/recommended"],
-    languageOptions: { globals: globals.node }
+    languageOptions: { globals: globals.node },
+    rules: {
+      "no-unused-vars": ["error", { "argsIgnorePattern": "^_" }]
+    }
   },
   {
     files: ["**/*.test.js"],
